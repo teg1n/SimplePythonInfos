30b139d891aafc92a9681c74fd04922a42f5209f
Ali Rızacan Köseoğlu <ali.koseoglu@gmail.com>
2024-07-23 23:37:22 +0300

    turtle added star and square

--- my projects/SimpleTurtleProject/TestTheTurt.py
+++ my projects/SimpleTurtleProject/TestTheTurt.py
@@ -1,4 +1,4 @@
-import turtle 
+"""import turtle 
 
 board = turtle.Screen()
 board.bgcolor("Light blue")
@@ -6,4 +6,38 @@ board.title("Turtle")
 
 turtle_test = turtle.Turtle()
 turtle.forward(150)
+turtle.done()
+
+
+#SQUARE 
+
+import turtle
+
+board= turtle.Screen()
+board.bgcolor("Light blue")
+board.title("Square")
+
+turtle_square = turtle.Turtle()
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+
+turtle.done()
+
+"""
+#STAR
+import turtle
+
+board=turtle.Screen()
+board.bgcolor("orange")
+
+turtle_star= turtle.Turtle()
+for i in range(5):
+    turtle.forward(100)
+    turtle.left(144)
+
 turtle.done()
